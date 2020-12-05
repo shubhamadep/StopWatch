a37db73c58e51aa3598ba9f38c71feb8b40efafd
Shubham <shubham.adep@yahoo.com>
2020-12-04 22:50:16 -0800

    Added two classes to implement stop watch functionality

---
+++ StopWatch/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace StopWatch
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Boolean stopWatchState = true;
+            var stopWatch = new StopWatchFunctions();
+
+            Console.WriteLine("Welcome to Stop Watch : \n");
+
+            Console.WriteLine("Press any key + enter to begin.. \n Press x + enter to end \n");
+
+            while(true)
+            {
+                var input = Console.ReadLine();
+
+                if(input == "x" || input == "X"){
+                    break;
+                }
+                else
+                {
+                    //sending reference of the variable to keep consistent state.
+                    stopWatch.ChangeState(ref stopWatchState);
+                }
+            }
+
+            Console.WriteLine("Thank you for using Stop Watch. \n");
+        }
+    }
+}
+
---
+++ StopWatch/StopWatchFunctions.cs
@@ -0,0 +1,38 @@
+﻿using System;
+namespace StopWatch
+{
+    public class StopWatchFunctions
+    {
+
+        private DateTime _startTime;
+        private DateTime _endTime;
+
+        public void ChangeState(ref Boolean state)
+        {
+            if (state)
+            {
+                StartStopWatch();
+                state = !state;
+            }
+            else
+            {
+                StopStopWatch();
+                state = !state;
+            }
+        }
+
+        void StartStopWatch()
+        {
+            Console.WriteLine("Starting stop watch. Press any key except x / X to stop. \n");
+            this._startTime = DateTime.Now;
+        }
+
+        void StopStopWatch()
+        {
+            this._endTime = DateTime.Now;
+            var totalTime = this._endTime.Subtract(this._startTime);
+            Console.WriteLine("Stop watch stopped, total time : " + totalTime.Seconds.ToString() + " seconds. \n");
+        }
+
+    }
+}
